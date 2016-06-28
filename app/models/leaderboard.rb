3c6b6901c3396f7dd86c71bfae4dfc89c7ada1f7
Yoni Doron-Peters | hybris <yoni.doron-peters@hybris.com>
2016-06-28 10:52:26 -0600

    Make today's activities query more efficient

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -3,29 +3,26 @@ class Leaderboard < ActiveRecord::Base
   # TODO return an entity of leaderboard and persist it to db. Then can easily aggregate totals for 2 days, 1 week, 1 month, etc.
   # or persist at 23:59 every night instead
   def Leaderboard.calc_todays_stats
-    athlete_to_activities = Hash.new
-    athletes              = Athlete.all
-    athletes.each do |athlete|
-      athlete_to_activities[athlete] = athlete.activities.where(created_at: Date.today.beginning_of_day..Date.today.end_of_day)
-    end
-    self.reduce_stats(athlete_to_activities)
+    todays_activities = Activity.where(created_at: Date.today.beginning_of_day..Date.today.end_of_day)
+    self.reduce_stats(todays_activities)
   end
 
-  def Leaderboard.reduce_stats(hsh)
-    athlete_totals = []
-    hsh.each do |athlete, activities|
-      beers    = 0
-      calories = 0
-
-      activities.each do |act|
-        beers    += act.beers
-        calories += act.calories
-      end if activities
-      athlete_totals << { name: athlete.name, img_url: athlete.img_url, beers: beers, calories: calories, id: athlete.id }
+  def Leaderboard.reduce_stats(activities)
+    athlete_totals = Hash.new
+    activities.each do |activity|
+      if athlete_totals[activity.athlete_id]
+        athlete_totals[activity.athlete_id].beers += activity.beers
+        athlete_totals[activity.athlete_id].calories += activity.calories
+      else
+        athlete = Athlete.find(activity.athlete_id)
+        athlete_totals[athlete.id] = { name: athlete.name, id: athlete.id, img_url: athlete.img_url,
+                                      beers: activity.beers, calories: activity.calories }
+      end
     end
     athlete_totals
+        .values
         .sort_by { |h| h[:beers] }
-        .reject { |h| h[:calories] == 0 }
+        .reject  { |h| h[:calories] == 0 }
         .reverse
   end
 
