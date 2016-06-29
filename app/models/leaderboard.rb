67c85341c695724c599a2103e37dda221e9c8ae6
Yoni Doron-Peters | hybris <yoni.doron-peters@hybris.com>
2016-06-29 09:29:50 -0600

    Change beer count to last 24 instead of 'today' due to popular demand until next feature is implemented.

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -3,7 +3,8 @@ class Leaderboard < ActiveRecord::Base
   # TODO return an entity of leaderboard and persist it to db. Then can easily aggregate totals for 2 days, 1 week, 1 month, etc.
   # or persist at 23:59 every night instead
   def Leaderboard.calc_todays_stats
-    todays_activities = Activity.where(created_at: Date.today.beginning_of_day..Date.today.end_of_day)
+    # TODO find out why Arthur Perry # Chicago Portage activity is not returned by query
+    todays_activities = Activity.where("created_at > ?", 1.day.ago) #created_at: Date.today.beginning_of_day..Date.today.end_of_day)
     self.reduce_stats(todays_activities)
   end
 
