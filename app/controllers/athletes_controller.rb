f8731a13c9f6c7ca22e12a2f2ec3b714ca8ccdbc
Yoni Doron-Peters | hybris <yoni.doron-peters@hybris.com>
2016-07-23 16:53:11 -0600

    Some overdue cleanup

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -1,5 +1,5 @@
 class AthletesController < ApplicationController
-  before_action :set_athlete, only: [:show, :edit, :update, :destroy]
+  before_action :set_athlete, only: [:show]
 
   # GET /athletes
   # GET /athletes.json
@@ -10,55 +10,6 @@ class AthletesController < ApplicationController
   # GET /athletes/1
   # GET /athletes/1.json
   def show
-  end
-
-  # GET /athletes/new
-  def new
-    @athlete = Athlete.new
-  end
-
-  # GET /athletes/1/edit
-  def edit
-  end
-
-  # POST /athletes
-  # POST /athletes.json
-  def create
-    @athlete = Athlete.new(athlete_params)
-
-    respond_to do |format|
-      if @athlete.save
-        format.html { redirect_to @athlete, notice: 'Athlete was successfully created.' }
-        format.json { render :show, status: :created, location: @athlete }
-      else
-        format.html { render :new }
-        format.json { render json: @athlete.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /athletes/1
-  # PATCH/PUT /athletes/1.json
-  def update
-    respond_to do |format|
-      if @athlete.update(athlete_params)
-        format.html { redirect_to @athlete, notice: 'Athlete was successfully updated.' }
-        format.json { render :show, status: :ok, location: @athlete }
-      else
-        format.html { render :edit }
-        format.json { render json: @athlete.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /athletes/1
-  # DELETE /athletes/1.json
-  def destroy
-    @athlete.destroy
-    respond_to do |format|
-      format.html { redirect_to athletes_url, notice: 'Athlete was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   #####################################################
--- app/views/activities/index.json.jbuilder
+++ app/views/activities/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@activities) do |activity|
-  json.extract! activity, :id, :name, :distance, :type, :moving_time, :total_elevation_gain, :calories, :start_lat, :start_long, :end_lat, :end_long, :kudos_count
+  json.extract! activity, :id, :name, :distance, :activity_type, :moving_time, :total_elevation_gain, :calories, :start_lat, :start_long, :end_lat, :end_long, :kudos_count
   json.url activity_url(activity, format: :json)
 end
